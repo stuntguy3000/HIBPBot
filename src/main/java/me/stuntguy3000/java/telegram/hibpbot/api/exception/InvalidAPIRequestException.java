b1a6cfd1b8a84ae2984d9ce76c0f284599abb775
Luke Anderson <stuntguy3000@gmail.com>
2016-06-08 22:07:32 +0930

    .gitignore is now working

--- src/main/java/me/stuntguy3000/java/telegram/hibpbot/api/exception/InvalidAPIRequestException.java
+++
@@ -1,8 +0,0 @@
-package me.stuntguy3000.java.telegram.hibpbot.api.exception;
-
-/**
- * @author stuntguy3000
- */
-public class InvalidAPIRequestException extends ApiException {
-
-}
--- src/main/java/me/stuntguy3000/java/telegram/hibpbot/api/exception/NoBreachesException.java
+++
@@ -1,7 +0,0 @@
-package me.stuntguy3000.java.telegram.hibpbot.api.exception;
-
-/**
- * @author stuntguy3000
- */
-public class NoBreachesException extends ApiException {
-}
